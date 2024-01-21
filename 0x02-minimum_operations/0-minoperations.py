8f2c57961ca899aa3b56a0253451e9f3f0e0e252
timoumi12 <ihebtimoumi1@gmail.com>
2024-01-21 15:44:33 +0100

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+""" Calculates the fewest number of operations needed,
+   to result in exactly n H characters in the file"""
+
 
 def minOperations(n):
     '''returns nb of ops if impossible => 0'''
 
     h = 'H'
     final = 'H'
-    l = len(final)
     op = 0
-    while(l < n):
-        if n % l == 0:
+    while(len(final) < n):
+        if n % len(final) == 0:
             op += 2
             h = final
             final += final
         else:
             op += 1
             final += h
-        l = len(final)
     if len(final) != n:
         return 0
     return op
