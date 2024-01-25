8878d9f49c470ccc1e485d033281d387ea7b402a
timoumi12 <ihebtimoumi1@gmail.com>
2024-01-25 23:31:28 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''determines if all the boxes can be opened'''
+
+def canUnlockAll(boxes):
+    '''Return True if all boxes can be opened, else return False'''
+
+    pos = 0
+    unlocked = {}
+    for b in boxes:
+        if(len(b) == 0 or pos == 0):
+            unlocked[pos] = "always unlocked"
+        for key in b:
+            if key < len(boxes) and key != pos:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        pos += 1
+    return False
