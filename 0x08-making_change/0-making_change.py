6893f45b421abb65ed97618d92353031828fa5cb
iheb <ihebtimoumi12@gmail.com>
2024-08-03 22:16:04 +0100

    solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,7 @@
 
 
 def makeChange(coins, total):
-    """determine the fewest number of coins needed to meet a given amount total"""
+    """determine the fewest number of coins needed to meet a given total"""
     if total <= 0:
         return 0
     coins.sort(reverse=True)
